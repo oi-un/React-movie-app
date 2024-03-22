0282bdc5a7d018d66166669c70838b021504cf62
yunji_choi <choiyj@kakao.com>
2024-03-22 16:01:02 +0900

    header, main 반응형

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,6 +46,42 @@ export default function Header({ searchMovie }) {
             />
             <i className="fa-solid fa-magnifying-glass"></i>
           </div>
+
+          <input type="checkbox" id="toggle-btn" />
+          <label htmlFor="toggle-btn" className="toggle-btn">
+            <i className="fa-solid fa-bars toggle-btn"></i>
+          </label>
+          <div className="toggle-menu">
+            <ul className="toggle-nav">
+              <li
+                className="nav-item active-item"
+                onClick={() => {
+                  searchMovie("now_playing");
+                }}
+              >
+                <a href="#">오늘의 상영작</a>
+              </li>
+              <li
+                className="nav-item"
+                onClick={() => {
+                  searchMovie("popular");
+                }}
+              >
+                <a href="#">인기 상영작</a>
+              </li>
+              <li
+                className="nav-item"
+                onClick={() => {
+                  searchMovie("top_rated");
+                }}
+              >
+                <a href="#">평점순</a>
+              </li>
+              <li>
+                <a href="#">검색</a>
+              </li>
+            </ul>
+          </div>
         </header>
       </div>
     </>
